7f29d9b8bbaf0acfdaee6458b2a85900090e5f20
Anand Vinod <anand.vinod.kodeclik@gmail.com>
2019-09-30 18:16:48 -0400

    Problem Number 1, Project Euler, Java HW

--- JavaHW/src/Multiples.java
+++ JavaHW/src/Multiples.java
@@ -1,2 +1,35 @@
 public class Multiples {
-}
+
+
+
+        public static void main(String[]Args){
+
+            int sum = 0;
+
+            for(int i = 0; i<1000; i++){
+
+                if(i%3 == 0){
+
+                    sum += i;
+
+                }
+
+                else if(i%5 == 0){
+
+                    sum +=i;
+
+                }
+
+                else {
+
+                    sum = sum;
+
+                }
+
+            }
+
+            System.out.println(sum);
+
+        }
+
+    }
